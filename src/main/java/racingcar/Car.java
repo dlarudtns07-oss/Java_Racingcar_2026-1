1ad3029bfaebb5ed86267a78cc6604b53957ab39
dlarudtns07-oss <dlarudtns07@naver.com>
2026-03-30 23:36:11 +0900

    자동차

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,9 @@
 package racingcar;
-
-public class Application {
+ {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 }
-package racingcar;
 
 public class Car {
     private final String name;
